201bb8ac6b7c27bd94b131ab08a7ea3a9dda7687
Kayokwa Chibuye <101683398+KayokwaC@users.noreply.github.com>
2025-04-08 21:59:23 +0000

    Add test data for Octofit app

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,12 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from django.core.management.base import BaseCommand
-from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from octofit_tracker.test_data import test_users, test_teams, test_activities, test_leaderboard, test_workouts
 from django.conf import settings
 from pymongo import MongoClient
-from datetime import timedelta
-from bson import ObjectId
 
 class Command(BaseCommand):
-    help = 'Populate the database with test data for users, teams, activity, leaderboard, and workouts'
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
 
     def handle(self, *args, **kwargs):
         # Connect to MongoDB
@@ -20,49 +21,30 @@ class Command(BaseCommand):
         db.leaderboard.drop()
         db.workouts.drop()
 
-        # Create users
-        users = [
-            User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='thundergodpassword'),
-            User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='metalgeekpassword'),
-            User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='zerocoolpassword'),
-            User(_id=ObjectId(), username='crashoverride', email='crashoverride@hmhigh.edu', password='crashoverridepassword'),
-            User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='sleeptokenpassword'),
-        ]
-        User.objects.bulk_create(users)
+        # Populate users
+        for user in test_users:
+            db.users.insert_one(user)
 
-        # Create teams
-        team = Team(_id=ObjectId(), name='Blue Team')
-        team.save()
-        team.members.add(*users)
+        # Populate teams
+        for team in test_teams:
+            members = [db.users.find_one({"username": member})["_id"] for member in team["members"]]
+            team["members"] = members
+            db.teams.insert_one(team)
 
-        # Create activities
-        activities = [
-            Activity(_id=ObjectId(), user=users[0], activity_type='Cycling', duration=timedelta(hours=1)),
-            Activity(_id=ObjectId(), user=users[1], activity_type='Crossfit', duration=timedelta(hours=2)),
-            Activity(_id=ObjectId(), user=users[2], activity_type='Running', duration=timedelta(hours=1, minutes=30)),
-            Activity(_id=ObjectId(), user=users[3], activity_type='Strength', duration=timedelta(minutes=30)),
-            Activity(_id=ObjectId(), user=users[4], activity_type='Swimming', duration=timedelta(hours=1, minutes=15)),
-        ]
-        Activity.objects.bulk_create(activities)
+        # Populate activities
+        for activity in test_activities:
+            user_id = db.users.find_one({"username": activity["user"]})["_id"]
+            activity["user"] = user_id
+            db.activity.insert_one(activity)
 
-        # Create leaderboard entries
-        leaderboard_entries = [
-            Leaderboard(_id=ObjectId(), user=users[0], score=100),
-            Leaderboard(_id=ObjectId(), user=users[1], score=90),
-            Leaderboard(_id=ObjectId(), user=users[2], score=95),
-            Leaderboard(_id=ObjectId(), user=users[3], score=85),
-            Leaderboard(_id=ObjectId(), user=users[4], score=80),
-        ]
-        Leaderboard.objects.bulk_create(leaderboard_entries)
+        # Populate leaderboard
+        for entry in test_leaderboard:
+            user_id = db.users.find_one({"username": entry["user"]})["_id"]
+            entry["user"] = user_id
+            db.leaderboard.insert_one(entry)
 
-        # Create workouts
-        workouts = [
-            Workout(_id=ObjectId(), name='Cycling Training', description='Training for a road cycling event'),
-            Workout(_id=ObjectId(), name='Crossfit', description='Training for a crossfit competition'),
-            Workout(_id=ObjectId(), name='Running Training', description='Training for a marathon'),
-            Workout(_id=ObjectId(), name='Strength Training', description='Training for strength'),
-            Workout(_id=ObjectId(), name='Swimming Training', description='Training for a swimming competition'),
-        ]
-        Workout.objects.bulk_create(workouts)
+        # Populate workouts
+        for workout in test_workouts:
+            db.workouts.insert_one(workout)
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
