d18f1c759424fc7e65cac879d8d862c0ab43fa29
Kayokwa Chibuye <101683398+KayokwaC@users.noreply.github.com>
2025-04-08 21:50:38 +0000

    Updated database engine to django.db.backends.sqlite3 and fixed related imports

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "djongo",
+        "ENGINE": "django.db.backends.sqlite3",
         "NAME": "octofit_db",
         "HOST": "localhost",
         "PORT": 27017,
